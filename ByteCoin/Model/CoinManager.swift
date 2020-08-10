250271a2c9e30b9cadf0614b69b225375cdf4c71
Jaronce Dutil <jaronce.d@gmail.com>
2020-08-10 18:12:33 -0400

    ByteCoin APII APP challenge complete

--- ByteCoin/Model/CoinManager.swift
+++ ByteCoin/Model/CoinManager.swift
@@ -8,12 +8,53 @@
 
 import Foundation
 
+protocol CoinManagerDelegate {
+    func didUpdatePrice(price: String, currency: String)
+    func didFailWithError(error: Error)
+}
+
 struct CoinManager {
     
+    var delegate: CoinManagerDelegate?
+    
     let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
-    let apiKey = "YOUR_API_KEY_HERE"
+    let apiKey = "F7B8350B-9E1C-4874-A74B-B8E508B43331"
     
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
 
+    func getCoinPrice(for currency: String) {
+        let urlString = "\(baseURL)/\(currency)?apikey=\(apiKey)"
+        if let url = URL(string: urlString) {
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    self.delegate?.didFailWithError(error: error!)
+                    return
+                }
+                
+                if let safeData = data {
+                    if let bitCoinPrice = self.parseJSON(currencyData: safeData) {
+                        let priceString = String(format: "%.2f", bitCoinPrice)
+                        self.delegate?.didUpdatePrice(price: priceString, currency: currency)
+                    }
+                }
+            }
+            task.resume()
+        }
+    }
     
+   
+    
+    func parseJSON(currencyData: Data) -> Double? {
+        let decoder = JSONDecoder()
+        do {
+            let decodedData = try decoder.decode(CoinData.self, from: currencyData)
+            let lastPrice = decodedData.rate
+            return lastPrice
+        } catch {
+            self.delegate?.didFailWithError(error: error)
+            return nil
+        }
+    }
+   
 }
